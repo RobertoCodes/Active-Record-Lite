3e8143b6e2aa1fdf6abb6dd62bb2a40d12edf6c9
Robert <robertandrewromano@gmail.com>
2016-03-16 02:21:00 -0400

    Add methods to initialize a new object and make getter/setter methods.

--- sql_object.rb
+++ sql_object.rb
@@ -2,6 +2,34 @@ require 'active_support/inflector'
 
 class SQLObject
 
+  def self.columns
+      if @columns
+        return @columns
+      else
+        query_result = DBConnection.execute2(<<-SQL)
+          SELECT
+            *
+          FROM
+            #{self.table_name}
+          LIMIT
+            0
+          SQL
+
+        @columns = query_result.first.map{|column| column.to_sym}
+      end
+  end
+
+  def self.finalize!
+    columns.each do |column|
+      define_method("#{column}") do
+        attributes[column]
+      end
+      define_method("#{column}=") do |value|
+        attributes[column] = value
+      end
+    end
+  end
+
   def self.table_name=(table_name)
     @table_name = table_name
   end
@@ -12,6 +40,21 @@ class SQLObject
     else
       self.to_s.tableize
     end
+  end
+
+  def initialize(params = {})
+    params.each do |attr_name, value|
+      attr_name.to_sym
+      if !self.class.columns.include?(attr_name)
+        raise "unknown attribute '#{attr_name}'"
+      else
+        send(attr_name.to_s + "=", value)
+      end
+    end
+  end
+
+  def attributes
+    @attributes ||= {}
   end
 
 end
