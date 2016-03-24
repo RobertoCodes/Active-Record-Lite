9f61f8e16a49172172a8ed8e3015e86f261054e0
Robert <robertandrewromano@gmail.com>
2016-03-24 05:38:04 -0400

    Add subclasses of AssocOptions for 'has many' and 'belongs to' relations.

--- associatable.rb
+++ associatable.rb
@@ -16,3 +16,19 @@ class AssocOptions
     model_class.table_name
   end
 end
+
+class BelongsToOptions < AssocOptions
+  def initialize(name, options = {})
+    options[:foreign_key] ? @foreign_key = options[:foreign_key] : @foreign_key = (name.to_s.underscore + "_id").to_sym
+    options[:primary_key] ? @primary_key = options[:primary_key] : @primary_key = :id
+    options[:class_name] ? @class_name = options[:class_name] : @class_name = name.to_s.camelcase
+  end
+end
+
+class HasManyOptions < AssocOptions
+  def initialize(name, self_class_name, options = {})
+    options[:foreign_key] ? @foreign_key = options[:foreign_key] : @foreign_key = (self_class_name.to_s.underscore + "_id").to_sym
+    options[:primary_key] ? @primary_key = options[:primary_key] : @primary_key = :id
+    options[:class_name] ? @class_name = options[:class_name] : @class_name = name.to_s.singularize.camelcase
+  end
+end
