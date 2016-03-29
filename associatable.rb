645561b75aa06544df2a6af77724fb7ab5bc8737
Robert <robertandrewromano@gmail.com>
2016-03-28 20:27:40 -0400

    Add 'has_one_through' relation to associatable module.

--- associatable.rb
+++ associatable.rb
@@ -34,6 +34,7 @@ class HasManyOptions < AssocOptions
 end
 
 module Associatable
+
   def belongs_to(name, options = {})
     options = BelongsToOptions.new(name, options)
 
@@ -51,6 +52,30 @@ module Associatable
     define_method(name) do
       primary_key = self.send(options.primary_key)
       options.model_class.where({options.foreign_key => primary_key})
+    end
+  end
+
+  def has_one_through(name, through_name, source_name)
+
+    define_method(name) do
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+      through_foreign_key = self.send(through_options.foreign_key)
+
+      results = DBConnection.execute(<<-SQL, through_foreign_key)
+        SELECT
+          #{source_options.table_name}.*
+        FROM
+          #{through_options.table_name}
+        JOIN
+          #{source_options.table_name}
+        ON
+          #{source_options.table_name}.#{source_options.primary_key} = #{through_options.table_name}.#{source_options.foreign_key}
+        WHERE
+          #{through_options.table_name}.#{through_options.primary_key} = ?
+        SQL
+
+        source_options.model_class.parse_all(results).first
     end
   end
 
